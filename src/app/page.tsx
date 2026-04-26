3656e4f93078be72143fb4d46d16ea0923f9c6e9
wcsdproducer <john@t3kniq.com>
2026-04-25 20:59:33 -0400

    fix: force dynamic on root page to prevent stale CDN html cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
-'use client';
-
 import { FirebaseClientProvider } from '@/firebase';
 import { TitleScreen } from '@/app/title-screen';
+
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
   return (
